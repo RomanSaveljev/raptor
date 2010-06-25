df22c937128a2f4e2bce5e6fc209ad2c0cd6a5fb
timothy murphy <timothy.murphy@nokia.com>
2010-06-25 20:59:06 +0100

    grokbuild: get the raptor version number, total raptor time is now "very nearly almost" correct (missing some very small and short invocations of raptor)

--- bin/grokbuild.py
+++ bin/grokbuild.py
@@ -25,6 +25,7 @@ import os
 import re
 from  optparse import OptionParser
 import annofile
+import datetime
 
 
 def checkconvert(dirname, filename):
@@ -78,10 +79,38 @@ class HeliumLog(object):
 class MainAntLog(HeliumLog):
 	# output/logs/92_7952_201020_003_main.ant.log
 	filenamesuffix = "_main.ant.log"
+	timeformat = "%Y/%m/%d %H:%M:%S:%f" # e.g. Thu 2010/06/24 09:15:42:625 AM
 
 	def __init__(self, logpath, buildid):
 		super(MainAntLog,self).__init__(logpath, buildid)
 
+		# Starting logging into y:\output\logs\mcl_7901_201024_20100623181534_main.ant.log at Wed 2010/06/23 21:16:12:972 PM
+		# Stopping logging into y:\output\logs\mcl_7901_201024_20100623181534_main.ant.log from hlm:record task at Thu 2010/06/24 09:15:42:625 AM
+
+		start_re = re.compile("Starting logging into [^ ]+ at ... ([^ ]+ +[^ ]+) .*")
+		stop_re = re.compile("Stopping logging into [^ ]+ from [^ ]+ task at ... ([^ ]+ +[^ ]+) (AM)|(PM).*")
+		start_time = None
+		stop_time = None
+		with open(self.logfilename) as f:
+			for l in f:
+				if start_time is None:
+					m = start_re.match(l)
+					if m:
+						#sys.stderr.write("start TIME: %s\n" %m.groups()[0])
+						start_time = datetime.datetime.strptime(m.groups()[0], self.timeformat)
+						
+				else: # if there are many stop lines then make sure the last one overrides the others
+					m = stop_re.match(l)
+					if m:
+						stop_time = datetime.datetime.strptime(m.groups()[0], self.timeformat)
+						#sys.stderr.write("stop TIME: %s\n" %m.groups()[0])
+
+		#sys.stderr.write("build start/stop: %s / %s  from %s\n" % (start_time, stop_time, self.logfilename))
+		build_duration = stop_time - start_time  # returns a timedelta object
+		self.build_duration = build_duration.seconds +  86400 * build_duration.days  # seconds
+
+	def __str__(self):
+		return "<metric name='build_duration'  value='%d'>\n" % self.build_duration
 		
 class AntEnvLog(HeliumLog):
 	# output/logs/92_7952_201020_003_ant_env.log
@@ -133,10 +162,8 @@ class RaptorAnnofile(object):
 
 
 class RaptorBuild(HeliumLog):
-	# mcl_7901_201024_20100623181534_dfs_build_ncp_variants.build_input_compile.log
-	# mcl_7901_201024_20100623181534_dfs_build_sf_variants.build_input_compile.log
 	def __init__(self, logpath, buildid, build):
-		self.filenamesuffix = '_%s.build_input_compile.log' % build
+		self.filenamesuffix = '_%s' % build
 		super(RaptorBuild,self).__init__(os.path.join(logpath, "compile"), buildid)
 		self.build = build
 
@@ -145,66 +172,74 @@ class RaptorBuild(HeliumLog):
 		run_time_re = re.compile("<info>Run time ([0-9]+) seconds</info>.*")
 		
 		emake_invocation_re = re.compile("<info>Executing.*--emake-annofile=([^ ]+) .*")
+		sbs_version_re = re.compile("<info>sbs: version ([^\n\r]*).*")
 		with open(self.logfilename) as f:
 			for l in f:
 				m = run_time_re.match(l)
 				if m:
-					self.build_duration = m.groups()[0]
+					self.build_duration = int(m.groups()[0])
 				
-				m2 = emake_invocation_re.match(l)
-				if m2:
-					(adir, aname) = os.path.split(m2.groups()[0])
+				m = emake_invocation_re.match(l)
+				if m:
+					(adir, aname) = os.path.split(m.groups()[0])
 					if aname.find("pp")==-1: # no parallel parsing ones preferably
-						print "found annotation file %s" % aname
+						sys.stderr.write("found annotation file %s\n" % aname)
 						self.annofile_names.append(os.path.join(logpath, "makefile", aname))
+
+				m = sbs_version_re.match(l)
+				if m:
+					self.version = m.groups()[0]
 
 		self.annofiles = []
 		for p in self.annofile_names:
 			self.annofiles.append(RaptorAnnofile(p, buildid))
 
 	def __str__(self):
-		return 	"<raptorbuild logfile='%s'>" % self.logfilename + \
-			"\n<metric name='raptor_duration_%s'  value='%s'>\n" % (self.build, self.build_duration) + \
+		return 	"<raptorbuild logfile='%s'>\n" % os.path.split(self.logfilename)[-1] + \
+			" <metric name='raptor_version'  value='%s'>\n" % (self.version) + \
+			" <metric name='raptor_duration_%s'  value='%d'>\n" % (self.build, self.build_duration) + \
 			"".join([str(a) for a in self.annofiles]) + \
 			"</raptorbuild>\n"
-
 		
 
 
 class HeliumBuild(object):
 	def __init__(self, logpath, buildid):
 		self.buildid = buildid
-		self.targettimes = 0
 		self.logpath = logpath
 		self.logfiles=[]
 
 	def __str__(self):
-		return  self.buildid + str(self.targettimes) + \
-			"\n"+[str(a) for a in self.annofiles]
+		return  self.buildid + \
+			"\n"+[str(a) for a in self.annofiles] + "\n"
 
 class Helium9Build(HeliumBuild):
 	def __init__(self, logpath, buildid):
 		super(Helium9Build,self).__init__(logpath, buildid)
 		self.mainantlog = MainAntLog(logpath, buildid)
-		#self.targettimes = TargetTimesLog(logpath, buildid)
 		self.raptorbuilds = []
 
 		# mcl_7901_201024_20100623181534_dfs_build_ncp_variants.build_input_compile.log
 		# mcl_7901_201024_20100623181534_dfs_build_sf_variants.build_input_compile.log
+		# mcl_7901_201024_20100623181534_dfs_build_winscw_dfs_build_winscw_input_compile.log
 		#
 		# ....but the problem is that the anno files have a slightly differning convention:
 		#        92_7952_201022_003_dfs_build_ncp_dfs_variants.resource_deps.emake.anno
 		#  _dfs_build_ncp_variants
 		#  _dfs_build_ncp_dfs_variants
-		for r in ["dfs_build_ncp_variants","dfs_build_sf_variants"]:
+                # read the annofile names from inside the raptor log output
+		for r in ["dfs_build_ncp_variants.build_input_compile.log","dfs_build_sf_variants.build_input_compile.log","dfs_build_winscw_dfs_build_winscw_input_compile.log", "ncp_symbian_build_symtb_input_compile.log"]:
 			self.raptorbuilds.append(RaptorBuild(logpath, buildid, r))
 
 
-
 	def __str__(self):
-		return "<heliumbuild ver='9' id='%s'>\n" % self.buildid + str(self.mainantlog) + \
-			str(self.targettimes) + \
-	 		"".join([str(a) for a in self.raptorbuilds ]) + "</heliumbuild>\n"
+
+		raptor_duration = reduce(lambda x, y: x + y,[y.build_duration for y in self.raptorbuilds],0)
+		return "<heliumbuild ver='9' id='%s'>\n" % (self.buildid) + \
+			"<metric name='total_duration'  value='%d'>\n" % (self.mainantlog.build_duration) + \
+			"<metric name='raptor_duration'  value='%d'>\n" % (raptor_duration) + \
+	 		"".join([str(a) for a in self.raptorbuilds ]) + \
+	 		"</heliumbuild>\n"
 		
 
 class HeliumLogDir(object):
@@ -236,7 +271,7 @@ if len(args) == 0:
 	print "Need at least one argument: a path to the logs."
 	sys.exit(-1)
 
-print "Gathering\n"
+sys.stderr.write("Gathering\n")
 #os.path.walk(args[0],visit,None)
 epocroot = args[0]
 
