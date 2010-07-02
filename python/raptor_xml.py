54cd99a01a3d2994896a6160273a8680cbc2d724
timothy murphy <timothy.murphy@nokia.com>
2010-07-02 14:17:26 +0100

    path not actually in localhost format, so  use "/" to join it

--- python/raptor_xml.py
+++ python/raptor_xml.py
@@ -389,7 +389,7 @@ class SystemModel(object):
 				if bldinf == None:
 					# recording layers containing non existent bld.infs
 					bldinfname = group.GetLocalString()
-					bldinfname = os.path.join(bldinfname,'bld.inf')
+					bldinfname = bldinfname+'/'+'bld.inf'
 					layer = self.__GetEffectiveLayer(aElement)
 					if not layer in self.__MissingBldInfs:
 						self.__MissingBldInfs[layer]=[]
