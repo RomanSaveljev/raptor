51ce9b298306b54a5b8117667f92f9f7c9f39bb0
Richard Taylor <richard.i.taylor@nokia.com>
2010-10-08 16:03:56 +0100

    test for per-component FLM

--- test/smoke_suite/flm_extension.py
+++ test/smoke_suite/flm_extension.py
@@ -18,14 +18,28 @@ from raptor_tests import SmokeTest
 
 def run():
 	t = SmokeTest()
-	t.id = "20"
-	t.name = "flm_extension"
-	t.command = "sbs -b " + \
-			"smoke_suite/test_resources/simple_extension/flm_bld.inf -c armv5"
+	
+	t.name = "exported_flm_extension"
+	t.command = "sbs -b smoke_suite/test_resources/simple_extension/flm_bld.inf -c armv5"
 	t.targets = [
 		"$(EPOCROOT)/epoc32/build/flm_test_1_2",
 		"$(EPOCROOT)/epoc32/tools/makefile_templates/tools/flm_export.xml",
 		"$(EPOCROOT)/epoc32/tools/makefile_templates/tools/flm_export.flm"
 		]
 	t.run()
+	
+	t.name = "per_component_flm"
+	t.command = "sbs --configpath=test/smoke_suite/test_resources/docs" + \
+	            " -b smoke_suite/test_resources/simple_dll/bld.inf" + \
+	            " -b smoke_suite/test_resources/simple_lib/bld.inf" + \
+	            " -c armv5.documentation"
+	t.targets = [
+		"$(EPOCROOT)/epoc32/build/flm_test_1_2",
+		"$(EPOCROOT)/epoc32/tools/makefile_templates/tools/flm_export.xml",
+		"$(EPOCROOT)/epoc32/tools/makefile_templates/tools/flm_export.flm"
+		]
+	t.run()
+	
+	t.name = "flm_extension"
+	t.print_result()
 	return t
