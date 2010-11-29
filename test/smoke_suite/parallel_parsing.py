96022d40adf4ea947e78bd69100926059f55c384
Daniel Jacobs <daniel.jacobs@nokia.com>
2010-11-29 14:56:57 +0000

    Extended test for the fix for SF Bug 2679.

--- test/smoke_suite/parallel_parsing.py
+++ test/smoke_suite/parallel_parsing.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+# Copyright (c) 2009 - 2010 Nokia Corporation and/or its subsidiary(-ies).
 # All rights reserved.
 # This component and the accompanying materials are made available
 # under the terms of the License "Eclipse Public License v1.0"
@@ -21,11 +21,7 @@ def run():
 	t.usebash = True
 	result = SmokeTest.PASS
 
-	description = """This test covers parallel parsing."""
-	command = "mkdir -p $(EPOCROOT)/epoc32/build && cd $(SBS_HOME)/test/smoke_suite/test_resources/pp/ && sbs --command=$(SBS_HOME)/test/smoke_suite/test_resources/pp/ppbldinf_commandfile -c armv5 -c winscw --pp=on --noexport -m ${SBSMAKEFILE} -f - | grep progress "
-
 	mmpcount = 10 # how many mmps in this parallel parsing test
-
 	
 	target_templ = [
 		"$(EPOCROOT)/epoc32/release/armv5/udeb/test_pp#.exe",
@@ -43,23 +39,36 @@ def run():
 		for atarget in target_templ:
 			targets.append(atarget.replace('pp#','pp'+ str(num)))
 
-	mustmatch = [
+	t.name = "parallel_parsing"
+	t.description = """This test covers parallel parsing."""
+	t.command = ("mkdir -p $(EPOCROOT)/epoc32/build && cd $(SBS_HOME)/test/smoke_suite/test_resources/pp/ && " 
+	"sbs --command=$(SBS_HOME)/test/smoke_suite/test_resources/pp/ppbldinf_commandfile -c armv5 -c winscw "
+	"--pp=on --noexport -m ${SBSMAKEFILE} -f - | grep progress ")
+	t.targets = targets
+	t.mustmatch =  [
 		".*<progress:start object_type='makefile' task='makefile_generation'.*"
 	]
-	mustnotmatch = [
+	t.mustnotmatch = [
 		".*<recipe .*name='makefile_generation_export.*",
 		".*<error[^><]*>.*"
 	]
+	t.warnings = 0
+	t.run()
 
-	warnings = 0
-		
-	t.id = "104"
-	t.name = "parallel_parsing"
-	t.description = description
-	t.command = command 
-	t.targets = targets
-	t.mustmatch = mustmatch
-	t.mustnotmatch = mustnotmatch
-	t.warnings = warnings
+	t.name = "parallel_parsing_missing_includes_in_bld_inf"
+	t.description = """ Ensure errors from parallel parsing are recorded by the top level Raptor. """
+	t.command = ("sbs -s smoke_suite/test_resources/pp/sys_def.xml -c armv5 -k --pp=on reallyclean > /dev/null 2>&1; "
+	"sbs -s smoke_suite/test_resources/pp/sys_def.xml -c armv5 -k --pp=on")
+	t.targets = []
+	t.mustmatch = [
+				"sbs: error: .*cpp.*test/smoke_suite/test_resources/pp/test01/bld.inf.*fatal error: this_file_does_not_exist.inf: No such file or directory",
+"sbs: error.*cpp.*compilation terminated.",
+"sbs: error: Preprocessor exception: ''Errors in .*test/smoke_suite/test_resources/pp/test01/bld.inf'' : in command.*cpp.*(component .*/test/smoke_suite/test_resources/pp/test01/bld.inf)",
+"sbs: error: .*cpp.*: .*/test/smoke_suite/test_resources/pp/test01/bld.inf:1: fatal error: this_file_does_not_exist.inf: No such file or directory"					
+					]
+	t.mustnotmatch = []
+	t.warnings = 5
+	t.errors = 9
+	t.returncode = 1
 	t.run()
 	return t
