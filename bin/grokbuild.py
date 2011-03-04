38a12f5d4ddfe0696787f94a31c8f9fbd0e02c59
Richard Taylor <richard.i.taylor@nokia.com>
2011-03-04 11:49:15 +0000

    add --output option to grokbuild

--- bin/grokbuild.py
+++ bin/grokbuild.py
@@ -31,12 +31,17 @@ parser = optparse.OptionParser(prog = "grokbuild",
 
 The build logs are usually in $EPOCROOT/output/logs""")
 
-parser.add_option("--maxagents", type="int", dest="maxagents", default=30,
+parser.add_option("--maxagents", type="int", default=30,
        help="The number of simultaneous agents used in the build. You need to "
             "supply this if --emake-class was used rather than --emake-maxagents "
             "since this is then a property of the build cluster and is not usually "
-            "recorded in the logs. The default is %default."
-       )
+            "recorded in the logs. The default is %default.")
+
+parser.add_option("--output", default="-",
+       help="The name of the output file to store the grok results in. A value "
+            "of '-' can be used to write to the standard output. The default "
+            "is '%default'.")
+
 (options, args) = parser.parse_args()
 
 if len(args) == 0:
@@ -47,4 +52,9 @@ epocroot = args[0]
 sys.stderr.write("Gathering Performance Metrics for %s\n" % epocroot)
 
 b = allo.helium.HeliumLogDir(epocroot, options)
-b.write(sys.stdout)
+
+if options.output == "-":
+	b.write(sys.stdout)
+else:
+	with open(options.output, "w") as stream:
+		b.write(stream)
