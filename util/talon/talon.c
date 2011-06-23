50cf3e0ad7cc64071f2b2c5d1d0d3ef527756463
Jon Chatten <devnull@localhost>
2011-06-23 18:13:37 +0100

    Bug 225 - Long list of command-line arguments chokes SBSv2

--- util/talon/talon.c
+++ util/talon/talon.c
@@ -49,6 +49,7 @@ sbs_semaphore talon_sem;
 #define TALON_ATTEMPT_STRMAX 32
 #define RECIPETAG_STRMAX 2048
 #define STATUS_STRMAX 120
+#define WARNING_STRMAX 250
 
 #define TALONDELIMITER '|'
 #define VARNAMEMAX 100
@@ -80,7 +81,7 @@ void prependattributes(buffer *b, char *attributes)
 	char *att;
 	
 	
-        strcpy(recipetag, "<recipe ");
+	strcpy(recipetag, "<recipe ");
 	rt = recipetag + 8;
 
 	att = attributes;
@@ -449,6 +450,7 @@ int main(int argc, char *argv[])
 
 	/* Talon can look in a flags variable to alter it's behaviour */
 	int force_success = 0;
+	int shell_cl_max = 0;
 	char *flags_str = talon_getenv("TALON_FLAGS");
 	if (flags_str)
 	{
@@ -464,6 +466,11 @@ int main(int argc, char *argv[])
 		{
 			dotagging = 0;
 		}
+
+		/* check command line lengths if a maximum is supplied */
+		char *shell_cl_max_str = strstr(flags_str, "shell_cl_max:");
+		if (shell_cl_max_str)
+			shell_cl_max = atoi(shell_cl_max_str + strlen("shell_cl_max:"));
 
 		free(flags_str); flags_str = NULL;
 	}	
@@ -532,13 +539,28 @@ int main(int argc, char *argv[])
 		
 		if (p) 
 		{
-			char status[STATUS_STRMAX];
-			char timestat[STATUS_STRMAX];
-
 			talon_returncode = p->returncode;
 
 			if (dotagging) 
 			{
+				char status[STATUS_STRMAX];
+				char timestat[STATUS_STRMAX];
+				char warning[WARNING_STRMAX];
+
+				bzero(warning, WARNING_STRMAX);
+
+				if (shell_cl_max)
+				{
+					int cl_actual = strlen(qrp);
+					if (cl_actual > shell_cl_max)
+						{
+						snprintf(warning, WARNING_STRMAX-1, \
+							"\n<warning>Command line length '%d' exceeds the shell limit on this system of '%d'.  " \
+							"If this recipe is a compile, try using the '.use_compilation_command_file' variant to reduce overall command line length.</warning>", \
+							cl_actual, shell_cl_max);
+						}
+				}
+
 				char *flagsstr = force_success == 0 ? "" : " flags='FORCESUCCESS'";
 				char *reasonstr = "" ;
 
@@ -562,6 +584,7 @@ int main(int argc, char *argv[])
 				buffer_append(p->output, "\n]]>", 4);
 				buffer_append(p->output, timestat, strlen(timestat));
 				buffer_append(p->output, status, strlen(status));
+				buffer_append(p->output, warning, strlen(warning));
 				buffer_append(p->output, "\n</recipe>\n", 11);
 			}
 		
