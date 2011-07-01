05deb7c01c41b2c5e16d2c0606182544b1d34c74
timothy murphy <timothy.murphy@nokia.com>
2011-07-01 15:23:11 +0100

    Allow make and dialog utility source to be downloaded to be built

--- util/dialog.mk
+++ util/dialog.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006-2009 Nokia Corporation and/or its subsidiary(-ies).
+# Copyright (c) 2006-2011 Nokia Corporation and/or its subsidiary(-ies).
 # All rights reserved.
 # This component and the accompanying materials are made available
 # under the terms of the License "Eclipse Public License v1.0"
@@ -23,6 +23,8 @@ RAPTOR_DIALOG_VER:=1.1-20080819
 DIALOG_SOURCEDIR:=$(OUTPUTPATH)/dialog-$(RAPTOR_DIALOG_VER)
 DIALOG_TAR:=$(SBS_HOME)/util/ext/dialog-$(RAPTOR_DIALOG_VER).tar.gz
 
+DIALOG_TAR_URL:=http://projects.developer.nokia.com/raptor/files/dialog-$(RAPTOR_DIALOG_VER).tar.gz
+
 
 define b_dialog
 
@@ -41,6 +43,14 @@ $(INSTALLROOT)/bin/dialog: $(DIALOG_TAR)
 	CFLAGS="-O3 $(GCCTUNE) -s" ./configure --prefix=$(INSTALLROOT) && \
 	$(MAKE) -j8 && $(MAKE) install \
 	)
+
+$(DIALOG_TAR):
+	for url in $(DIALOG_TAR_URL); do \
+    	wget $(DIALOG_TAR_URL) -O $(DIALOG_TAR); \
+    	if [ $$? -eq 0 ]; then break; fi ;\
+	done
+
+
 endef
 
 $(eval $(b_dialog))
--- util/make.mk
+++ util/make.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006-2009 Nokia Corporation and/or its subsidiary(-ies).
+# Copyright (c) 2006-2011 Nokia Corporation and/or its subsidiary(-ies).
 # All rights reserved.
 # This component and the accompanying materials are made available
 # under the terms of the License "Eclipse Public License v1.0"
@@ -19,6 +19,7 @@ RAPTOR_MAKE_VER:=3.81
 
 MAKE_SOURCEDIR:=$(OUTPUTPATH)/make-$(RAPTOR_MAKE_VER)
 MAKE_TAR:=$(SBS_HOME)/util/ext/make-$(RAPTOR_MAKE_VER).tar.bz2
+MAKE_TAR_URL:=http://mirrors.kernel.org/gnu/make/make-$(RAPTOR_MAKE_VER).tar.bz2  http://www.nic.funet.fi/pub/gnu/ftp.gnu.org/pub/gnu/make/make-$(RAPTOR_MAKE_VER).tar.bz2
 
 
 define b_make
@@ -28,6 +29,12 @@ define b_make
 all:: make
 
 make: $(INSTALLROOT)/bin/make
+
+$(MAKE_TAR):
+		for url in $(MAKE_TAR_URL); do \
+		    wget $(MAKE_TAR_URL) -O $(MAKE_TAR); \
+			if [ $$? -eq 0 ]; then break; fi ;\
+		done
 	
 $(INSTALLROOT)/bin/make: $(MAKE_TAR) 
 	rm -rf $(MAKE_SOURCEDIR) && \
