e3cdbd29f65251e5d239d21c9b410b1f4c2668b2
Stefan Karlsson <stefan.karlsson@nokia.com>
2011-09-12 18:10:34 +0100

    Remove POSTLINKER_SUPPORTS_ASMTYPE conf. variable

--- test/smoke_suite/custom_dll.py
+++ test/smoke_suite/custom_dll.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+# Copyright (c) 2010-2011 Nokia Corporation and/or its subsidiary(-ies).
 # All rights reserved.
 # This component and the accompanying materials are made available
 # under the terms of the License "Eclipse Public License v1.0"
@@ -11,16 +11,26 @@
 #
 # Contributors:
 #
-# Description: 
+# Description:
 
 
-from raptor_tests import SmokeTest 
+from raptor_tests import SmokeTest
 
 def run():
+
 	t = SmokeTest()
-	t.name = "custom_dll"
 	t.usebash = True
-	t.command = "SBS_ELF2E32=$SBS_HOME/test/smoke_suite/test_resources/custom_dll/elf2e32/windows/elf2e32.exe  sbs -b smoke_suite/test_resources/custom_dll/bld.inf -c armv5 --configpath=$SBS_HOME/test/smoke_suite/test_resources/custom_dll/config"
+
+	BLD_INF = "smoke_suite/test_resources/custom_dll/bld.inf"
+	OLD_ELF2E32 = "$(SBS_HOME)/test/smoke_suite/test_resources/custom_dll/elf2e32_old"
+	if t.onWindows:
+		OLD_ELF2E32 += ".exe"
+
+	# Commands for using the new (i.e. the one with the --asm option) and old
+	# version of elf2e32.
+	new_cmd = "sbs -b {0} -c {{0}}".format(BLD_INF)
+	old_cmd = "SBS_ELF2E32={0}".format(OLD_ELF2E32) + " " + new_cmd
+
 	t.targets = [
 		"$(EPOCROOT)/epoc32/release/armv5/lib/customdll.dso",
 		"$(EPOCROOT)/epoc32/release/armv5/lib/customdll{000a0000}.dso",
@@ -29,6 +39,21 @@ def run():
 		"$(EPOCROOT)/epoc32/release/armv5/urel/customdll.dll",
 		"$(EPOCROOT)/epoc32/release/armv5/urel/customdll.dll.map"
 		]
-	# Windows-only until we know about a suitable linux version of the post-linker
-	t.run("windows")
+	t.addbuildtargets(BLD_INF, [
+		"customdll_dll/armv5/customdll{000a0000}.",
+		"customdll_dll/armv5/customdll{000a0000}.exp"])
+
+	t.name = "custom_dll_rvct_new_elf2e32"
+	t.command = new_cmd.format("armv5")
+	t.run()
+
+	t.name = "custom_dll_rvct_old_elf2e32"
+	t.command = old_cmd.format("armv5")
+	t.run()
+
+	t.name = "custom_dll_gcce_new_elf2e32"
+	t.command = new_cmd.format("gcce_armv5")
+	t.run()
+
+	t.name = "custom_dll"
 	return t
